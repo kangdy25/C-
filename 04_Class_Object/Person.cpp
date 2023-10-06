a5fa473741aec94a7db4b05769a072af15a14768
Kangdy <canfrog@naver.com>
2023-10-06 17:25:28 +0900

    Edit Code of 04

--- 04_Class_Object/Person.cpp
+++ 04_Class_Object/Person.cpp
@@ -26,3 +26,16 @@ void Person::chAddr(const char* newAddr) {
 	addr = new char[strlen(newAddr)+1];
 	strcpy(addr, newAddr);
 }
+
+int main() {
+	Person* p1 = new Person("강동윤", "서울시 강동구");
+	Person* p2 = new Person("데이빗", "마다가스카르");
+	p1->print();
+	p2->print();
+	cout << endl << "주소 변경 : ";
+	p2->chAddr("남양주 화도");
+	p2->print();
+	delete p1;
+	delete p2;
+	return 0;
+}
